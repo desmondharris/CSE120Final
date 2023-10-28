1d00cdc55cb7d50c4d383ddef1255dd2d63b1ec8
Desmond Harris <desmondktharris@gmail.com>
2023-10-27 20:17:00 -0400

    Add lexing and skeleton of parse

--- src/Parser.py
+++ src/Parser.py
@@ -1,6 +1,50 @@
+from Core import *
+import logging
 class Symbol:
-
-
     def __init__(self, variable: str, value: float=0):
         self.variable = variable
         self.value = value
+
+
+def lex(expression: str):
+    tokens = []
+    for l in expression:
+        if l.isdigit():
+            tokens.append(float(l))
+        elif is_basic_operand(l):
+            tokens.append(l)
+        else:
+            logging.warning(f"Unknown character '{l}'")
+    return tokens
+
+
+def parse(expression: str):
+    tokens = lex(expression)
+
+    return tokens
+
+
+class Parser:
+    def __init__(self):
+        pass
+
+    def evaluate(self, expression: str):
+        for l in expression:
+            if l == ' ':
+                pass
+            elif l == '+':
+                pass
+            elif l == '-':
+                pass
+            elif l == '*':
+                pass
+            elif l == '/':
+                pass
+            elif l == '^':
+                pass
+            elif l == '(':
+                pass
+            elif l == ')':
+                pass
+            else:
+                pass
